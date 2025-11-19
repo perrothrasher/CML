bb3cf6364396b524ade83115178b5127bdedb765
perrothrasher <franciscoreyes199@gmail.com>
2025-11-19 11:30:09 -0300

    inicio train.py

---
+++ train.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+import scipy.stats
+import statsmodels.formula.api as sm
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
